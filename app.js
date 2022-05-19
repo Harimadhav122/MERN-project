720f8dc9ec760c5ee1ecb17b35b5fbb8215722d3
hari <harimadhav122@gmail.com>
2022-05-19 19:24:50 +0530

    fifth

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const mongodbUrl = "mongodb://localhost:27017";
-//const mongodbUrl = "mongodb://mongo:27017/nodeapp";
+//const mongodbUrl = "mongodb://localhost:27017";
+const mongodbUrl = "mongodb://mongo:27017/nodeapp";
 const empRouter = require("./routes/employeesRoute");
 const seqRouter = require("./routes/sequencesRoute");
 const app = express();
@@ -14,9 +14,9 @@ const con = mongoose.connection;
 
 con.on("open", () => console.log("Mongodb Connected"));
 
-//app.listen(3000, () => console.log("Server running"));
+app.listen(3000, () => console.log("Server running"));
 
-app.listen(8080, () => console.log("Server running"));
+//app.listen(8080, () => console.log("Server running"));
 
 app.use((req, res) => {
     const error = new Error("Url not found");
